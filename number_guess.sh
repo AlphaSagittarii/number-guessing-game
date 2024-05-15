d3467a1b51efcd444f274778cfe350fb80bab8d3
AlphaSagittarii G <mg.esec@intelligence-airbusds.de>
2024-05-15 14:49:26 +0000

    feat: updated db for basic functionality

--- number_guess.sh
+++ number_guess.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
-RANDOM_NUMBER=(($RANDOM % 1000))
+PSQL="psql --username=freecodecamp --dbname=number_guessing_game -t --no-align -c"
+RANDOM_NUMBER=$(( (RANDOM % 1000) + 1 ))
+echo $RANDOM_NUMBER
 
 echo "Enter your username:"
 read USER_NAME
 
 # check if user exists
-USER_EXISTS=$($PSQL "")
+USER_EXISTS=$($PSQL "SELECT * FROM users WHERE user_name = '$USER_NAME'")
 if [[ -z $USER_EXISTS ]]
 then
   # user doesn't exist
-  Welcome, $USER! It looks like this is your first time here.
+  echo "Welcome, $USER_NAME! It looks like this is your first time here."
 else
   # user exists
-  echo Welcome back, $USER! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
+  echo Welcome back, $USER_NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
 fi
 
 # guess the number
 echo "Guess the secret number between 1 and 1000:"
 read GUESS
-while $RANDOM_NUMBER != $GUESS
+while [[ $RANDOM_NUMBER != $GUESS ]]
 do
   # if number higher than guess
-  if [[ $GUESS > $RANDOM_NUMBER ]]
+  if [[ $GUESS -gt $RANDOM_NUMBER ]]
   then
     echo "It's lower than that, guess again:"
     read GUESS
   #if number lower than guess
-  else if [[ $GUESS < $RANDOM_NUMBER ]]
+  elif [[ $GUESS -lt $RANDOM_NUMBER ]]
   then
-    echo "It's lower than that, guess again:"
+    echo "It's higher than that, guess again:"
     read GUESS
   # if number not int
-  else if [[ ! $GUESS =~ ^[0-9]+$ ]]
+  elif [[ ! $GUESS =~ ^[0-9]+$ ]]
   then
     echo "That is not an integer, guess again:"
-  # if number guessed
-  else
-   echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
   fi 
 done
+# if number guessed
+
+echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
